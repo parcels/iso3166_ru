c36f75fe06891b480baa05f636572515f0e14815
Artem Shitov <inbox@artemshitov.ru>
2013-05-03 14:25:44 +0400

    Country finder

--- lib/iso3166_ru.rb
+++ lib/iso3166_ru.rb
@@ -1,5 +1,10 @@
 require "iso3166_ru/version"
+require "iso3166_ru/country"
 
 module Iso3166Ru
-  # Your code goes here...
+  def self.find_by(query)
+    countries = File.open(File.expand_path("../iso3166_ru/data/countries.dat", __FILE__)) { |f| Marshal.load f }
+    query = query.to_a.flatten
+    countries.select { |e| e.send(query[0]) == query[1] }.first
+  end
 end
